12e9c749d228b5b3ce16f3016fb05ff8de1716e2
CojocaruCristian <51245957+cojocaruCristian@users.noreply.github.com>
2019-11-06 02:54:21 +0200

    lesson-14

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let x = prompt("enter value of x");
+let n = prompt("enter value of n");
+let p = Math.pow(x, n);
+
+let mainH1 = document.getElementById("mainH1").innerHTML = p;
+alert("Result of x =" + " " + x + " " + "in power of n =" + " "  + n + " " + "equals" + " " + p);
+
+
+
+
+
+
+
+
+
+
+
+
+
